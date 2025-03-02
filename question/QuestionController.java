c0ad7298fe7610c520d4279a97ff31692ced0f3b
JungHoon Lee <105867850+MeritEnding@users.noreply.github.com>
2025-03-02 21:34:35 +0900

    #1 feat: 조회수 기능 추가

--- question/QuestionController.java
+++ question/QuestionController.java
@@ -46,7 +46,8 @@ public class QuestionController {
 	
 	@GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
-    	Question question = this.questionService.getQuestion(id);
+    	this.questionService.viewUp(id);
+		Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
     	
         return "question_detail";
