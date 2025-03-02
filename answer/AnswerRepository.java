f7ed849c521915c2b369e67e9bea0d188e92af9a
JungHoon Lee <105867850+MeritEnding@users.noreply.github.com>
2025-03-02 23:26:56 +0900

    #1 feat: 답변 정렬 기능추가

--- answer/AnswerRepository.java
+++ answer/AnswerRepository.java
@@ -2,6 +2,12 @@ package com.mysite.sbb.answer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+import com.mysite.sbb.question.Question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+	Page<Answer> findByQuestion(Question question, Pageable pageable);
 }
